d24c6490a8f6027bf43ac9655a518e7e359fbcf4
ktf <not.discostoo@gmail.com>
2013-05-25 21:13:15 -0700

    add exercise 1.11

---
+++ exercise_1.11.scm
@@ -0,0 +1,21 @@
+; If n < 3, return n. Otherwise return f(n-1) + 2f(n-2) + 3f(n-3). Ugh.
+(define (dumb-func n)
+  (if (< n 3)
+    n
+    (+ (dumb-func (- n 1))
+       (* 2 (dumb-func (- n 2)))
+       (* 3 (dumb-func (- n 3)))
+    )
+  )
+)
+
+
+; Testing:
+(display (dumb-func 1))
+(newline)
+(display (dumb-func 2))
+(newline)
+(display (dumb-func 3))
+(newline)
+(display (dumb-func 4))
+(newline)
